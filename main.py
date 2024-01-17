471ad27eb336308482411f8b9e31beac653c0a03
selma001 <selmatareb@gmail.com>
2024-01-17 16:41:22 +0100

    finale commit

--- main.py
+++ main.py
@@ -1,16 +1,60 @@
-from find import Find
+import numpy as np
+from math import inf
+from fonctionalities import Fonctionalities
+import time
+from tqdm import tqdm
+from search import Search
 import cv2
 
-THRESHOLD = 50
+class Find:
 
+    def with_dichotomic(im1, im2, threshold=50, size=16, decalage=64):
+
+        image1 = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
+        image2 = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
+        image1 = cv2.copyMakeBorder(
+            image1,
+            decalage,
+            decalage,
+            decalage,
+            decalage,
+            cv2.BORDER_CONSTANT,
+            None,
+            0
+        )
+        list_of_blocs = []
+        x1, y1 = image2.shape
+
+        print("[Searching]...")
+        start = time.time()
+        for i in tqdm(range(0, x1, size)):
+            for j in range(0, y1, size):
+                target_bloc = image2[i:i+size, j:j+size]
+                if target_bloc.shape == (size, size):
+                    _, x, y, min_mse = Search.dichotomique_search(
+                        target_bloc, image1, i, j, size, decalage)
+                    if min_mse < threshold:
+                        list_of_blocs.append((x-decalage, y-decalage, i, j))
+
+        end = time.time()
+
+        print(f"[Result] in {end-start}s")
+
+        print(f"[Preparing] image similarities ...")
+        Fonctionalities.show_similarities(im1.copy(), list_of_blocs, size, mode="log")
+        print(f"[Done]")
+
+        print(f"[Preparing] image residues ...")
+        residue_image = Fonctionalities.show_residues(im2, list_of_blocs, size, mode="log")
+        print(f"[Done]")
+
+        
+        print(f"[Finish]") 
+
+
+#---- main test ----#
+THRESHOLD = 50
 img1 = cv2.imread("image072.png")
 img2 = cv2.imread('image092.png')
 
-choice = input("\nType de recherche ?\n -1) Lineaire\n -2) Logarithmique\n")
-
-algorithm = {
-    '1': Find.with_sliding,
-    '2': Find.with_dichotomic,
-}
-
-algorithm[choice](img1, img2, THRESHOLD, DELTA=64)
+Find.with_dichotomic(img1, img2, THRESHOLD)
--- search.py
+++ search.py
@@ -3,78 +3,48 @@ from math import inf
 
 class Search:
 
-    @staticmethod
     def MSE(bloc_1, bloc_2):
         x, y = bloc_1.shape
         difference = np.square(bloc_1-bloc_2)
         return np.sum(difference)/(x*y)
 
-    @staticmethod
-    def dichotomique_search(target_bloc, searching_image, x, y, BS=16, DELTA=64) -> np.ndarray:
+    def dichotomique_search(target_bloc, searching_image, x, y, size=16, decalage=64) -> np.ndarray:
 
-        step = 32
+        pas = 32
         min_mse = inf
 
-        coord_x = x+DELTA
-        coord_y = y+DELTA
+        coord_x = x+decalage
+        coord_y = y+decalage
 
         min_x = coord_x
         min_y = coord_y
 
         min_bloc = None
 
-        while step >= 1:
-            MOUVEMENTS = [(0, 0), (step, 0), (-step, 0), (0, step), (-step, 0),
-                          (step, step), (-step, -step), (step, -step), (-step, step)]
+        while pas >= 1:
+            MOUVEMENTS = [(0, 0), (pas, 0), (-pas, 0), (0, pas), (-pas, 0),
+                          (pas, pas), (-pas, -pas), (pas, -pas), (-pas, pas)]
 
             for move in MOUVEMENTS:
-                n, m = move
-                coord_x += n
-                coord_y += m
-                current_bloc = searching_image[coord_x:coord_x +
-                                               BS, coord_y:coord_y+BS]
-                if target_bloc.shape == current_bloc.shape:
-                    temp_mse = Search.MSE(target_bloc, current_bloc)
-                    if temp_mse < min_mse:
-                        min_mse = temp_mse
+                a, s = move # x dans a et y dans s
+                coord_x += a
+                coord_y += s
+                bloc = searching_image[coord_x:coord_x + size, coord_y:coord_y+size]
+
+                if target_bloc.shape == bloc.shape:
+
+                    mse = Search.MSE(target_bloc, bloc)
+                    if  mse < min_mse:
+                        min_mse = mse
                         min_x = coord_x
                         min_y = coord_y
-                        min_bloc = current_bloc
+                        min_bloc = bloc
 
-                coord_x -= n
-                coord_y -= m
+                coord_x -= a
+                coord_y -= s
 
-            step //= 2
+            pas //= 2
 
         return min_bloc, min_x, min_y, min_mse
 
-    @staticmethod
-    def sliding_search(target_bloc, searching_image, x, y, x2, y2, dx=7, dy=7, BS=16, DELTA=64) -> np.ndarray:
-
-        WIDTH, HEIGHT = BS, BS
-        DX, DY = dx, dy
-
-        new_x, new_y = x-DX, y-DY
-        new_x2, new_y2 = x2+DX, y2+DY
-
-        search_zone = searching_image[new_x +
-                                      DELTA:new_x2+DELTA, new_y+DELTA:new_y2+DELTA]
-
-        min_mse = inf
-        min_bloc = None
-
-        x_b2 = 0
-        y_b2 = 0
-
-        for n in range(search_zone.shape[0]):
-            for m in range(search_zone.shape[1]):
-                current_bloc = search_zone[n:n+HEIGHT, m:m+WIDTH]
-                if current_bloc.shape == target_bloc.shape:
-                    temp_mse = Search.MSE(target_bloc, current_bloc)
-                    if min_mse > temp_mse:
-                        min_mse = temp_mse
-                        x_b2 = n
-                        y_b2 = m
-                        min_bloc = current_bloc
-
-        return search_zone, min_bloc, x_b2, y_b2, min_mse
+    
